116fe7552c8bc46bea42c24537408ce98d608255
FMGordillo facundo@blockfi.com <facundomgordillo@gmail.com>
2021-02-14 10:03:41 -0300

    :sparkles::lipstick: Hope you like it

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import { ApolloServer } from "apollo-server";
+import { ApolloServer } from "apollo-server-express";
+import express, { Request, Response } from "express";
 import "reflect-metadata";
 import * as tq from "type-graphql";
 import { context } from "./context";
@@ -7,24 +8,14 @@ import { resolvers } from "./generated/typegraphql-prisma";
 const PORT = process.env.PORT ?? 3000;
 
 const initServer = async () => {
-  /**
-   * Acá es donde debería mostrarles esto:
-   * @see https://github.com/prisma/prisma-examples/blob/latest/typescript/graphql/src/schema.ts
-   * ... pero me parece que no.
-   *
-   * En su lugar, les muestro la mejor solución que encontré para mi caso
-   * type-graphql && typegraphql-prisma
-   *
-   * NOTAS IMPORTANTES:
-   *
-   * - No olvides `npx prisma generate`, o no va a encontrar los 'resolvers'
-   * - Fijate que agregamos dos lineas en tsconfig.json,
-   *   eso es porque `typegraphql-prisma` usa '@Decoradores'
-   *   y otras "cositas" de Typescript que ignoro
-   *   @see https://github.com/MichalLytek/typegraphql-prisma/issues/58
-   * - Se instalaron nuevas dependencias porque type-graphql lo necesita
-   *   @see https://github.com/MichalLytek/typegraphql-prisma/blob/main/Readme.md
-   */
+  const app = express();
+
+  app.get("/", (_req: Request, res: Response) => {
+    res.send(
+      `Server running with Express! Oh sorry, you want the GraphQL Playgroud? Try http://localhost:${PORT}/graphql`
+    );
+  });
+
   const schema = await tq.buildSchema({
     resolvers,
     validate: false,
@@ -35,14 +26,13 @@ const initServer = async () => {
     context,
   });
 
-  server
-    .listen(PORT)
-    .then(() =>
-      console.log(
-        `Prisma + Apollo + GraphQL Server listening on http://localhost:${PORT}`
-      )
+  server.applyMiddleware({ app });
+
+  app.listen(PORT, () =>
+    console.log(
+      `Prisma + Apollo + Express + GraphQL Server listening on http://localhost:${PORT}`
     )
-    .catch((error) => console.log(`Error while initializing Apollo server`));
+  );
 };
 
 initServer();
